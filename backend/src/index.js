c8c1d07abfe06eb603bb9d67d1dfbe1b595ad408
Denys Menfredy <denys.menfredy7@gmail.com>
2021-01-23 16:20:12 -0300

    environment variables added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,18 +3,28 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const http = require('http');
-
-const {setupWebSocket} = require('./webSocket');
+require('dotenv').config({path: __dirname + '/.env'});
+const { setupWebSocket } = require('./webSocket');
 
 const app = express();
 const server = http.Server(app);
 
 setupWebSocket(server);
 
+const atlasUser = process.env.ATLAS_USER;
+const atlasPassword = process.env.ATLAS_PASSWORD;
+// console.log(atlasPassword);
+
 // MongoDB (Não-relacional)
-mongoose.connect('mongodb+srv://denysm7:xky4k2nrwr@cluster0-usaxi.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${atlasUser}:${atlasPassword}@cluster0-usaxi.mongodb.net/omnistack10?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("Database Connected");
 });
 
 app.use(cors());
